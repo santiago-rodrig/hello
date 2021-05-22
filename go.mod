fca79b54ea8cb65807fefe7d0366eb623ea08ead
Santiago Rodriguez <santo1996.29@gmail.com>
2021-05-21 19:44:45 -0600

    add main code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/santiago-rodrig/hello
+
+go 1.16
+
+require github.com/santiago-rodrig/greetings v0.0.0-20210522013424-13646249e885
---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/santiago-rodrig/greetings"
+)
+
+func main() {
+	fmt.Println(greetings.Hello("Santiago"))
+}
